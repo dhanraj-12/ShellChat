8815fcf2be3690982cf8589b7dff48437035da0a
Dhanraj Jadhav <dj004786@gmail.com>
2024-10-17 12:10:29 +0530

    adding server and client

--- server.cpp
+++ server.cpp
@@ -1,39 +1,63 @@
-#include<bits/stdc++.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
+#include <bits/stdc++.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring> // For memset
+
 using namespace std;
 
 int main() {
-    //creating socket
-    int serverSocket = socket(AF_INET,SOCK_STREAM,0);
+    // Creating socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        cerr << "Failed to create socket." << endl;
+        return -1;
+    }
 
-    // Specifying the address_information
-
+    // Specifying the address information
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
 
-    // Binding the socket and adress info
+    // Binding the socket to the address
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        cerr << "Bind failed." << endl;
+        close(serverSocket);
+        return -1;
+    }
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    // Listening on the assigned socket
+    if (listen(serverSocket, 5) == -1) {
+        cerr << "Listen failed." << endl;
+        close(serverSocket);
+        return -1;
+    }
 
-    // listen to assinged socket
+    cout << "Server is listening on port 8080..." << endl;
 
-    listen(serverSocket, 5);
-    
-    // accepting the connection request
+    // Accepting the connection request
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == -1) {
+        cerr << "Failed to accept connection." << endl;
+        close(serverSocket);
+        return -1;
+    }
 
-    int clientSocket = accept(serverSocket,nullptr,nullptr);
-
-    // recieving data
-
+    // Receiving data
     char buffer[1024] = {0};
+    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1) {
+        cerr << "Failed to receive data." << endl;
+        close(clientSocket);
+        close(serverSocket);
+        return -1;
+    }
 
-    recv(clientSocket, buffer, sizeof(buffer), 0);
     cout << "Message from client: " << buffer << endl;
 
+    // Closing sockets
+    close(clientSocket);
     close(serverSocket);
 
     return 0;
